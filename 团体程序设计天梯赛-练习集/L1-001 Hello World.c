48681410a5d7c3039f09c175281c5982d62924d3
by5388 <730305388@qq.com>
2018-10-13 00:15:30 +0800

    move
    move file

---
+++ 团体程序设计天梯赛-练习集/L1-001 Hello World.c
@@ -0,0 +1,18 @@
+/*
+L1-001 Hello World （5 分）
+这道超级简单的题目没有任何输入。
+
+你只需要在一行中输出著名短句“Hello World!”就可以了。
+
+输入样例：
+无
+输出样例：
+Hello World!
+*/
+#include<stdio.h>
+int main() {
+
+	printf("Hello World!");
+	return 0;
+}
+
