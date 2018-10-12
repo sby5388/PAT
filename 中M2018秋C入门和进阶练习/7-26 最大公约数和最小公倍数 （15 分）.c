b5b10c0fe76641d079d10dd543a848a7fa5d4017
sby5388 <by5388@github.com>
2018-10-12 15:28:19 +0800

    ubbits

--- 中M2018秋C入门和进阶练习/7-26 最大公约数和最小公倍数 （15 分）.c
+++ 中M2018秋C入门和进阶练习/7-26 最大公约数和最小公倍数 （15 分）.c
@@ -15,49 +15,43 @@
 */
 
 
-
-
-
-
-
-
-
-
-
-
-
 #include<stdio.h>
 int main() {
-	int N,M;
-	scanf("%d %d",&N,&M);
-	
-	
+	int M,N;
+	scanf("%d %d",&M,&N);
+	if(M*N==0) {
+		return 0;
+	}
+
+	if(M==N) {
+		printf("%d %d",N,M);
+		return 0;
+	}
+	if(M<N) {
+		int temp=M;
+		M=N;
+		N = temp;
+	}
+	// let M > N
+
+	if(M%N==0) {
+		printf("%d %d",N,M);
+		return 0;
+	}
+
+
+	int max = N;
+	while(max>1) {
+		if(0==M%max && 0==N%max) {
+			break;
+		}
+		max--;
+	}
+
+	int min = M*N/max;
+
+	printf("%d %d",max,min);
 	return 0;
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
