887d879c846ea4a10458d6e13c0693cbf99bb128
sby5388 <by5388@github.com>
2018-10-12 20:28:57 +0800

    add
     下班前

--- 基础编程题目集/6-11 求自定类型元素序列的中位数 （25 分）.c
+++ 基础编程题目集/6-11 求自定类型元素序列的中位数 （25 分）.c
@@ -1,6 +1,6 @@
 /*
 6-11 求自定类型元素序列的中位数 （25 分）
-本题要求实现一个函数，求N个集合元素A[]的中位数，即序列中第?N/2+1?大的元素。其中集合元素的类型为自定义的ElementType。
+本题要求实现一个函数，求N个集合元素A[]的中位数，即序列中第[N/2+1]大的元素。其中集合元素的类型为自定义的ElementType。
 
 函数接口定义：
 ElementType Median( ElementType A[], int N );
@@ -25,15 +25,17 @@ int main () {
 	int N, i;
 
 	scanf("%d", &N);
-	for ( i=0; i<N; i++ )
+	for ( i=0; i<N; i++ ) {
 		scanf("%f", &A[i]);
+	}
 	printf("%.2f\n", Median(A, N));
 
 	return 0;
 }
 
 /* 你的代码将被嵌在这里 */
-typedef float ElementType{
-	
-};
+ElementType Median( ElementType A[], int N ) {
+	//TODO 如何对C 的数组进行排序
+}
+
 
