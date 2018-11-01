e88819b7506e12b062258077975713176a8c5e09
by5388 <730305388@qq.com>
2018-11-02 00:21:43 +0800

    add

---
+++ temp/未命名1.c
@@ -0,0 +1,23 @@
+/*
+
+*/
+#include<stdio.h>
+int main() {
+//	int N;
+//	scanf("%d",&N);
+	int result  =1 + 2 + (3 + 4) * ((5 * 6 % 7 / 8) - 9) * 10;
+//	printf("%d",result);
+	printf("%d",((5 * 6 % 7 / 8) ));
+	int n=10;
+	if(1<=n<=9) {
+		printf("%d",88);
+	}
+
+	int i,j,k;
+	i=5;
+	j=10;
+	k=1;
+	printf("%d", k>i<j);
+	return 0;
+}
+
